4fa1aa3bf288a3242a776044d79511728674ac3e
Jackson <jpwoods92@gmail.com>
2025-03-22 02:04:41 -0400

    setup docker stuff

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,11 +12,11 @@ import { MessageModule } from './modules/message.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
+      host: String(process.env.DATABASE_HOST),
       port: parseInt(process.env.DATABASE_PORT as string),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      username: String(process.env.DATABASE_USER),
+      password: String(process.env.DATABASE_PASSWORD),
+      database: String(process.env.DATABASE_NAME),
       autoLoadEntities: true,
       synchronize: true,
       retryAttempts: 10,
